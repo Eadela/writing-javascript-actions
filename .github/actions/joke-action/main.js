105e9378ae2f9476667663caf17f57eaf8216600
Eadela <zhiqing.wang@brgroup.com>
2022-11-01 19:14:40 +0800

    Update main.js

--- .github/actions/joke-action/main.js
+++ .github/actions/joke-action/main.js
@@ -3,7 +3,9 @@ const core = require("@actions/core");
 
 async function run() {
   const joke = await getJoke();
+  console.log(11);
   console.log(joke);
+  console.log(33);
   core.setOutput("joke-output", joke);
 }
 
